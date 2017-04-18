493dd6359e45021c8bfeedeed97b1dd52aac77ee
Lynn Xu <LynnHui819@gmail.com>
2017-04-17 19:31:12 -0700

    Initial Commit

---
+++ QRScanner/QRScanner.h
@@ -0,0 +1,19 @@
+//
+//  QRScanner.h
+//  QRScanner
+//
+//  Created by 李玲 on 4/17/17.
+//  Copyright © 2017 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRScanner.
+FOUNDATION_EXPORT double QRScannerVersionNumber;
+
+//! Project version string for QRScanner.
+FOUNDATION_EXPORT const unsigned char QRScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRScanner/PublicHeader.h>
+
+
